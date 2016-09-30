61b1df43f4410b9b10904e23473b94ee814a388f
unknown <01158130@DS-IT-01109732.sf.com>
2016-09-30 18:07:38 +0800

    初始化enjoy-domain

---
+++ src/main/java/com/enjoy/management/domain/ManagerAdmin.java
@@ -0,0 +1,42 @@
+package com.enjoy.management.domain;
+
+public class ManagerAdmin {
+	private long id;
+	private String userName;
+	private String mobile;
+	private String mail;
+	private String password;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
